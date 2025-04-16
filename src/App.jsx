cd6b472f3bb27de118593404b6fe843eec5555cc
shadowcracks <yst187business@gmail.com>
2025-04-16 05:55:46 -0400

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
           <Route path="/contact" element={<Contact1 />} />
           <Route path="/error" element={<ErrorPage />} />
           <Route
-            path="/signin"
+            path="/"
             render={() => {
               loginWithRedirect();
             }}
